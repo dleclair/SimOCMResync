e6540c919778019e206a28a8db1346ce1f37e21e
Denis Leclair <denis@theleclairs.ca>
2014-10-01 12:40:28 -0400

    model is working for success path

--- SimOCMResync.cc
+++ SimOCMResync.cc
@@ -5,31 +5,41 @@
 #include <pthread.h>
 #include <iostream>
 #include <iomanip>
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
 
 
 using namespace std;
 
 
-void print_time()
+void print(string s)
 {
     struct timespec ts;
+    static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
+    pthread_mutex_lock (&mutex);
     clock_gettime(CLOCK_REALTIME, &ts);
     cout << ts.tv_sec % 100000 << "." << setfill('0') << setw(3) << ts.tv_nsec / 1000000;
+    cout << ": " << s << endl;
+    pthread_mutex_unlock(&mutex);
 }
 
 
 
 class Switch;
+class Software;
 
 class FPGA
 {
 public:
     FPGA(Switch *s)
     {
+        sw = NULL;
         optoswitch = s;
         last_port = cur_port = next_port = 0;
     };
+
+    Software *sw;
 
     void data_ready_interrupt();
 
@@ -55,7 +65,7 @@ private:
 class OCM 
 {
 public:
-    OCM(FPGA *f): port_scanned(0), fpga(f) {};
+    OCM(FPGA *f, Switch *sw): port_scanned(0), fpga(f), optoswitch(sw) {};
 
     void start();
 
@@ -74,6 +84,44 @@ private:
     }
 
     FPGA *fpga;
+    Switch *optoswitch;
+    timer_t timer;
+};
+
+
+
+class Software 
+{
+public:
+    Software()
+    {
+        mScanSwitchState = mSwitchState = 0;
+    };
+
+    void start();
+
+    OCM *ocm;
+    FPGA *fpga;
+
+    void data_ready_interrupt();
+    pthread_t thread;
+
+private:
+    void mainloop();
+
+    static void *startthread(void *arg)
+    {
+        Software *sw = static_cast<Software *>(arg);
+        sw->mainloop();
+    }
+
+    int setOpticalSwitchNext(int );
+    int getOpticalSwitchLast(int );
+
+    pthread_cond_t condvar;
+    pthread_mutex_t mutex;
+    int mScanSwitchState;
+    int mSwitchState;
 };
 
 
@@ -90,13 +138,22 @@ void OCM::start()
 
 void OCM::mainloop()
 {
+    boost::asio::io_service io;
+    boost::asio::deadline_timer timer(io, boost::posix_time::milliseconds(500));
+
     for (;;)
     {
+        timer.wait();
+        timer.expires_from_now(boost::posix_time::milliseconds(500));
+
         data_valid = false;
         data_ready = false;
 
         //sweep the spectrum
-        //get port number
+        stringstream s;
+        port_scanned = optoswitch->port;
+        s << " OCM: scanning port " << port_scanned;
+        print(s.str());
         usleep(10000);  //10ms to sweep the spectrum
 
         //process the data
@@ -106,11 +163,11 @@ void OCM::mainloop()
 
         //data is ready
         data_ready = true;
-        print_time();
-        cout << ": OCM : data_ready for port " << port_scanned << endl;
+        s.str("");
+        s.clear();
+        s << " OCM : data_ready for port " << port_scanned;
+        print(s.str());
         fpga->data_ready_interrupt();
-
-        usleep(500000);
     }    
 }
 
@@ -125,9 +182,12 @@ void FPGA::data_ready_interrupt()
     cur_port = next_port;
 
     //interrupt software
-    print_time();
-    cout << ": FPGA : last=" << last_port << ", cur=" << cur_port << ", next=" <<
-        next_port << ". interrupt to software." << endl;
+    stringstream s;
+    s << " FPGA : last=" << last_port << ", cur=" << cur_port << ", next=" <<
+        next_port << ". interrupt to software.";
+    print(s.str());
+    if (sw)
+        sw->data_ready_interrupt();
 
     //strobe switch with cur_port;
     optoswitch->strobe(cur_port);
@@ -141,20 +201,137 @@ void Switch::strobe(int new_port)
 {
     usleep(10000);
     port = new_port;
-    print_time();
-    cout << ": Switch : strobed to port " << port << endl;
+    stringstream s;
+    s << " Switch : strobed to port " << port;
+    print(s.str());
 }
 
+
+/**************************************************************************************/
+/* Software class definitions
+/**************************************************************************************/
+void Software::data_ready_interrupt()
+{
+    pthread_mutex_lock(&mutex);
+    pthread_cond_signal(&condvar);
+    pthread_mutex_unlock(&mutex);
+}
+
+void Software::start()
+{
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&condvar, NULL);    
+
+    //bind this software object to the fpga.
+    fpga->sw = this;
+
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_create(&thread, &attr, &Software::startthread, static_cast<void *>(this));
+}
+
+void Software::mainloop()
+{
+    for (;;)
+    {
+        pthread_mutex_lock(&mutex);
+        pthread_cond_wait(&condvar, &mutex);
+        pthread_mutex_unlock(&mutex);
+
+        usleep(10000);  //simulate delay in receiving pulse
+
+        stringstream s;
+        s << " Software : received interrupt pulse";
+        print(s.str());
+
+        mScanSwitchState = mSwitchState;
+        mSwitchState = setOpticalSwitchNext(mSwitchState);
+        mScanSwitchState = getOpticalSwitchLast(mScanSwitchState);
+
+        //read the data from the OCM
+        usleep(100000);
+        int ocm_port = ocm->port_scanned;
+        s.str("");
+        s.clear();
+        s << " Software: expecting data for " << mScanSwitchState << ", received " << ocm_port;
+        print(s.str());
+
+        if (ocm_port != mScanSwitchState)
+        {
+            s.str("");
+            s.clear();
+            s << " ERROR!!!";
+            print(s.str());
+            exit(0);
+        }
+    }
+}
+
+int Software::setOpticalSwitchNext(int state)
+{
+    int currentState, nextState;
+    bool resync = false;
+
+    nextState = currentState = state;
+
+    do
+    {
+        currentState = (currentState + 1) % 5;
+
+        if (currentState != fpga->cur_port)
+        {
+            stringstream s;
+            s << "setOpticalSwitchNext: warm reset detected. resyncing to cur_port: " << fpga->cur_port;
+            print(s.str());
+            resync = true;
+        }
+        else
+            resync = false;
+    } while (resync);
+
+    nextState = (currentState + 1) % 5;
+    fpga->next_port = nextState;
+    return currentState;
+}
+
+
+int Software::getOpticalSwitchLast(int state)
+{
+    int lastState = state;
+    bool resync = false;
+
+    do
+    {
+        if (lastState != fpga->last_port)
+        {
+            stringstream s;
+            s << "getOpticalSwitchLast: warm reset detected. resyncing to last_port: " << fpga->last_port;
+            print(s.str());
+            resync = true;
+            lastState = (lastState + 1) % 5;            
+        }
+        else
+            resync = false;
+    } while (resync);
+
+    return lastState;
+}
 
 
 int main()
 {
     Switch *sw = new Switch();
     FPGA *fpga = new FPGA(sw);
-    OCM *ocm = new OCM(fpga);
+    OCM *ocm = new OCM(fpga, sw);
 
     ocm->start();
 
+    Software *software = new Software();
+    software->ocm = ocm;
+    software->fpga = fpga;
+    software->start();
+
     pthread_join(ocm->thread, NULL);
+    pthread_join(software->thread, NULL);
     return 0;
 }
